ec8b1952fabd44cc13580c065eaa6f477385b4ff
Dharuan Luigi <dharuanluigi@gmail.com>
2024-03-04 21:02:45 -0300

    Change: fields of transactions to fit with system requirements

---
+++ Migrations/20240304235429_UpdateTransactionFieldTypes.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace r24q1.Migrations
+{
+    public partial class UpdateTransactionFieldTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Transactions",
+                type: "enum('d','c')",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(1)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Transactions",
+                type: "varchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Transactions",
+                type: "varchar(1)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "enum('d','c')");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Transactions",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(10)",
+                oldMaxLength: 10);
+        }
+    }
+}
